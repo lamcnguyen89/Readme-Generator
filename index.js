3731721662eecf982eda935e7d541b87ddbcc43e
Lam Nguyen <lamcnguyen89@gmail.com>
2020-07-04 04:08:41 -0400

    Created function to generate readme data using string literals. Created function to prompt user. Created function to do licensing. Created function to initialize all other functions.

--- index.js
+++ index.js
@@ -1,122 +1,159 @@
 // Call in the required packages for this JavaScript Application:
 
         // fs is a Node standard library package for reading and writing files
-        var fs = require("fs");
+        const fs = require("fs");
 
         // Inquirer is a Node library for asking questions, parsing the input, validating answers and managing heirarchal prompts
-        var inquirer = require("inquirer");
+        const inquirer = require("inquirer");
 
+        // Util is a node library
+        const util = require("util");
 
+        // For writing generated data to a file
+        const writeFileAsync = util.promisify(fs.writeFile);
 
+// Function to ask the user questions, then based off responses, generate the README.md file by running various functions.
 
-// array of questions for user
-        const questions = [
-                {
-                    // the title of the readme
-                    type: "input",
-                    name: "title" ,
-                    message: "What is the title of your README.md file?" 
-                },
-                {
-                    // Description of the project
-                    type: "input",
-                    name: "description",
-                    message: "Enter a description for your project. A hint would be to write it in a text editor and copy that text into this input."
-                },
-                {
-                    // Installation Instructions
-                    type: "input",
-                    name: "installation",
-                    message: " What are the prerequisite installation(s) required to run your application?"
-                },
-                {
-                    // Usage Information
-                    type: "input",
-                    name: "usage",
-                    message: "Provide instructions and examples for use. Include screenshots if necessary."
-                },
-                {
-                    // Contribution Guidelines
-                    // I will have to read the Contributor Covenant: https://www.contributor-covenant.org to make different questions
-                    type: "input",
-                    name: "contribution" ,
-                    message: "If you want other developers to contribute to your application, add guidelines on how to do this"
-                },
-                {
-                    // Test Instructions
-                    type: "input",
-                    name: "test" ,
-                    message: "Go the extra mile if you want and write tests for your application. Then provide examples of how to run them."
-                },
-                {
-                    // Choose License from list of licenses.
-                    type: "list",
-                    name: "license",
-                    message: "From the list below, choose the type of license you want for your application. To get help deciding on a license visit: http://www.choosealicense.com",
-                    choices: [
-                        "MIT License (Default open source license)",
-                        "GNU GPLv3",
-                        "GNU AGPLv3",
-                        "GNU LGPLv3",
-                        "Mozilla Public License 2.0",
-                        "Apache License 2.0",
-                        "Boost Software License 1.0",
-                        "The Unlicense"
-                    ]
-                },
-                {
-                    // Ask the user his github username.
-                    type: "input",
-                    name: "github",
-                    message: "What is your github username?"
-                },
-                {
-                    // Ask user's email address
-                    type: "input",
-                    name: "email",
-                    message: "What is your email?"
-                }
-        ];
+        function promptUser() {
+            return inquirer.prompt(
+                [
+                    {
+                        // the title of the readme
+                        type: "input",
+                        name: "title" ,
+                        message: "What is the title of your README.md file?" 
+                    },
+                    {
+                        // Description of the project
+                        type: "input",
+                        name: "description",
+                        message: "Enter a description for your project. A hint would be to write it in a text editor and copy that text into this input."
+                    },
+                    {
+                        // Installation Instructions
+                        type: "input",
+                        name: "installation",
+                        message: " What are the prerequisite installation(s) required to run your application?"
+                    },
+                    {
+                        // Usage Information
+                        type: "input",
+                        name: "usage",
+                        message: "Provide instructions and examples for use. Include screenshots if necessary."
+                    },
+                    {
+                        // Contribution Guidelines
+                        // I will have to read the Contributor Covenant: https://www.contributor-covenant.org to make different questions
+                        type: "input",
+                        name: "contributing" ,
+                        message: "If you want other developers to contribute to your application, add guidelines on how to do this"
+                    },
+                    {
+                        // Test Instructions
+                        type: "input",
+                        name: "test" ,
+                        message: "Go the extra mile if you want and write tests for your application. Then provide examples of how to run them."
+                    },
+                    {
+                        // Choose License from list of licenses.
+                        type: "list",
+                        name: "license",
+                        message: "From the list below, choose the type of license you want for your application. To get help deciding on a license visit: http://www.choosealicense.com",
+                        choices: [
+                            "MIT License (Default open source license)",
+                            "GNU GPLv3",
+                            "GNU AGPLv3",
+                            "GNU LGPLv3",
+                            "Mozilla Public License 2.0",
+                            "Apache License 2.0",
+                            "Boost Software License 1.0",
+                            "The Unlicense"
+                        ]
+                    },
+                    {
+                        // Ask the user his github username.
+                        type: "input",
+                        name: "github",
+                        message: "What is your github username?"
+                    },
+                    {
+                        // Ask user's email address
+                        type: "input",
+                        name: "email",
+                        message: "What is your email?"
+                    }
+            ]);
+        }
 
-// Actions for the program to take to Generate Readme:
+// Function to show Licensing:
 
+        function licensing(answers) {
+
+        }
+
+// Function to generate Markdown file for Readme. Use template literals:
+
+function generateMarkdown(answers) {
         // Display Title of the Readme:
-
         // Display sections of the Readme entitled Description, Installation, Usage, Contributing and Tests:
-
         // Display a badge for the license that is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under:
-
         // Add Github user name with a link to Github profile under the section titled Questions:
-
         // Generate 1 badge specific to the repository
-
         // Add user's email address to the section titled Questions, with instructions on how to reach for additional questions:
-        
         // Generate Table of Contents. When user clicks on lnks in the TOC, user is taken to corresponding section of the README
 
-// Method of the program:
-        // Call the required Modules
-        // Build the object that contains questions and answers based off User inputs using the Inquirer Module
-        // Create a blank markdown file called Readme using fs.writeFile
-        // Create a function that will write each line of the object using some sort of loop maybe a for loop or foreach loop.
-        // Within the For Loop, Use an ansychronous method using fs to append to the previously created Readme.md file. 
+        return `
 
-// Hints (These are the assignments I can look up to help do this homework):
-        // Week 05 Day 03: 00-40-Stu_Mini_Project
-        // Week 05 Day 01: 14-Ins_Inquirer-Demo
-        // Week 04 Day 05: 05-Ins_readFile
-         // Week 04 Day 05: 06-Ins_writeFile
-         // Week 04 Day 05: 07-Stu_appendFile
-            
+        # ${answers.title}
 
+        ## Description
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+        ${answers.description}
+
+        ## Installation
+
+        ${answers.installation}
+
+        ## Usage 
+
+        ${answers.usage}
+
+        ## License
+
+        ${}
+
+        ## Contributing
+
+        ${answers.contributing}
+
+        ## Tests
+
+        ${answers.test}
+
+        ## Questions
+
+        For questions, email me at: ${answers.email}
+        Or visit my Github
+        
+        
+        `
+
+};
 
 // function to initialize program
-function init() {
+async function init() {
+    try {
+        const answers = await promptUser()
 
+        const mdfile = generateMarkdown(answers);
+
+        await writeFileAsync("README.md", mdfile);
+
+        console.log("Successfully created Readme file")
+    } 
+    catch(err) {
+        console.log(err);
+    }
 }
 
 // function call to initialize program
