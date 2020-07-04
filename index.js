22e31c857915136f512856997a7286a4b56a40bd
Lam Nguyen <lamcnguyen89@gmail.com>
2020-07-03 22:41:23 -0400

    Added some questions to ask the user and commented out more steps to clarify the process of building this application.

--- index.js
+++ index.js
@@ -10,30 +10,65 @@
 
 
 // array of questions for user
-const questions = [
-
-    // the title of the readme
-    
-    // Description of the project
-
-    // Installation Instructions
-
-    // Usage Information
-
-    // Choose License from list of licenses.
-
-    // Contribution Guidelines
-
-    // Test Instructions
-
-    // Questions: 
-        // Ask the user his github username.
-        // Ask use email address
-  
-
-];
+        const questions = [
+                {
+                    // the title of the readme
+                    type: "input",
+                    name: "title" ,
+                    message: "What is the title of your README.md file?" 
+                },
+                {
+                    // Description of the project
+                    type: "input",
+                    name: "description",
+                    message: "Enter a description for your project. A hint would be to write it in a text editor and copy that text into this input."
+                },
+                {
+                    // Installation Instructions
+                    type: "input",
+                    name: "installation",
+                    message: " What are the prerequisite installation(s) required to run your application?"
+                },
+                {
+                    // Usage Information
+                    type: "input",
+                    name: "usage",
+                    message:
+                },
+                {
+                    // Contribution Guidelines
+                    type: "input",
+                    name: "contribution" ,
+                    message:
+                },
+                {
+                    // Test Instructions
+                    type: "input",
+                    name: "test" ,
+                    message:
+                },
+                {
+                    // Choose License from list of licenses.
+                    type: "list",
+                    name: "license",
+                    message: "From the list below, choose the type of license you want for your application."
+                },
+                {
+                    // Ask the user his github username.
+                    type: "input",
+                    name: "github",
+                    message: "What is your github username?"
+                },
+                {
+                    // Ask user's email address
+                    type: "input",
+                    name: "email",
+                    message: "What is your email?"
+                }
+        ];
 
 // Actions for the program to take to Generate Readme:
+
         // Display Title of the Readme:
 
         // Display sections of the Readme entitled Description, Installation, Usage, Contributing and Tests:
@@ -42,9 +77,26 @@ const questions = [
 
         // Add Github user name with a link to Github profile under the section titled Questions:
 
+        // Generate 1 badge specific to the repository
+
         // Add user's email address to the section titled Questions, with instructions on how to reach for additional questions:
         
         // Generate Table of Contents. When user clicks on lnks in the TOC, user is taken to corresponding section of the README
+
+// Method of the program:
+        // Call the required Modules
+        // Build the object that contains questions and answers based off User inputs using the Inquirer Module
+        // Create a blank markdown file called Readme using fs.writeFile
+        // Create a function that will write each line of the object using some sort of loop maybe a for loop or foreach loop.
+        // Within the For Loop, Use an ansychronous method using fs to append to the previously created Readme.md file. 
+
+// Hints (These are the assignments I can look up to help do this homework):
+        // Week 05 Day 03: 00-40-Stu_Mini_Project
+        // Week 05 Day 01: 14-Ins_Inquirer-Demo
+        // Week 04 Day 05: 05-Ins_readFile
+         // Week 04 Day 05: 06-Ins_writeFile
+         // Week 04 Day 05: 07-Stu_appendFile
+            
 
 
 // function to write README file
